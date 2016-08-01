c230ce2f97af01acdd2ada94f8d668dd1e3a8266
Sconnda <srao@ucvts.tec.nj.us>
2016-08-01 11:37:13 -0400

    Minor Edit

--- js/scroll.js
+++ js/scroll.js
@@ -14,7 +14,7 @@ $(function() {
 });
 $(window).scroll(function() {
 	if ($(window).width() > 768) {
-		if ($(document).scrollTop() > $(window).height()) {
+		if ($(document).scrollTop() > $(window).height()-50) {
 			$("#circle").fadeOut("fast");
 			$("#ucvtsLogo").fadeOut("fast");
 			$("#magnetLogo").fadeOut("fast");
